38f85c1b9c7816a9473d806f9c19e3e9e80cb040
OGabrielPereira <ogabrielpereira@pm.me>
2022-07-25 00:50:32 -0300

    booleans

---
+++ 03_booleans.pl
@@ -0,0 +1,16 @@
+use strict;
+use warnings;
+
+# undef, 0, 00000, "0", ""
+# Everything else is true
+
+my $statement = "fdfsdf";
+
+
+if ($statement) {
+    print "True\n";
+} else {
+    print "False\n";
+}   
+
+
